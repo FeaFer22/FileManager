81936f347941281541a3eb5a8a6141e3f00d42cd
FeaFer22 <msoleev98@gmail.com>
2022-06-04 16:15:43 +0300

    Создание структуры проекта, шаблона MVVM

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using FileManager.ViewModels.Base;
+
+namespace FileManager.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Заголовок окна
+        private string _Title = "File Manager";
+        
+        ///<summary>
+        ///Заголовок окна
+        ///</summary>
+        public string Title
+        {
+            get => _Title;
+            set
+            {
+                Set(ref _Title, value);
+            }
+        }
+
+        #endregion
+    }
+}
