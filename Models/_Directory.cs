64921bd22f0dc9dfc9245635e21f8b9cbabedde0
FeaFer22 <msoleev98@gmail.com>
2022-06-08 22:41:20 +0300

    Добавление модели, начало разработки функциональности

---
+++ Models/_Directory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager.Models
+{
+    internal class File
+    {
+        public string Name { get; set; }
+        public DateTime DateChanged { get; set; }
+        public string Size { get; set; }
+
+        public string Type { get; set; }
+    }
+    internal class _Directory
+    {
+        public string Name { get; set; }
+        public DateTime DateChanged { get; set; }
+        public ICollection<File> Files { get; set; }
+    }
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,9 @@
 ﻿using FileManager.Infrastructure.Commands;
+using FileManager.Models;
 using FileManager.ViewModels.Base;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
 using System.Windows;
 using System.Windows.Input;
 
@@ -7,9 +11,14 @@ namespace FileManager.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        /*---------------------------------------------------------------------------------------------------------------*/
+
+        private DirectoryInfo Dir;
+        private DirectoryInfo[] _Dirs;
+
         #region Заголовок окна
 
-        private string _Title = "File Manager";
+        private string _Title = "Файловый менеджер";
         ///<summary>
         ///Заголовок окна
         ///</summary>
@@ -23,31 +32,72 @@ namespace FileManager.ViewModels
         }
 
         #endregion
-        
+
+        #region Путь
+        private string _Path = "C:\\";
+        ///<summary>
+        ///Путь
+        ///</summary>
+        public string Path
+        {
+            get => _Path;
+            set
+            {
+                Set(ref _Path, value);
+            }
+        }
+
+        #endregion
+
+        ///<summary>
+        ///Директория
+        ///</summary>
+        public DirectoryInfo[] Dirs
+        {
+            get => _Dirs;
+            set
+            {
+                Set(ref _Dirs, value);
+            }
+        }
+
+
+
+        /*---------------------------------------------------------------------------------------------------------------*/
+
         #region Команды
-        
+
         #region CloseApplicationCommand
 
         public ICommand CloseApplicationCommand { get; }
 
         private bool CanCloseApplicationCommandExecute(object obj) => true;
 
-        private void OnCloseApplicationCommandExecuted(object obj) 
-        {
-            Application.Current.Shutdown();
-        }
+        private void OnCloseApplicationCommandExecuted(object obj) => Application.Current.Shutdown();
 
         #endregion
-        
+
         #endregion
 
+        /*---------------------------------------------------------------------------------------------------------------*/
         public MainWindowViewModel()
         {
             #region Команды
 
             CloseApplicationCommand = new ActionCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
-            
+
             #endregion
+
+            Dir = new DirectoryInfo(_Path);
+            _Dirs = Dir.GetDirectories();
+
+            var Dirs = new ObservableCollection<_Directory>();
+
+            foreach (DirectoryInfo currentDir in _Dirs)
+            {
+                Dirs.Add(new _Directory { Name = currentDir.Name, DateChanged = currentDir.LastWriteTime});
+            }
+
         }
     }
 }
